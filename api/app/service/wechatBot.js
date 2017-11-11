6e0a4300eb8a6091c10a7fae3472c33afce22e3f
mumudeveloper <mumudeveloper@outlook.com>
2017-11-12 01:03:36 +0800

    update

--- api/app/service/wechatBot.js
+++ api/app/service/wechatBot.js
@@ -14,9 +14,13 @@ module.exports = app => {
         const data = { isLogin: false };
         if (bot_list[user_id]) {
           data.isLogin = bot_list[user_id].state == bot_list[user_id].CONF.STATE.login;
-          data.uuid = bot_list[user_id].uuid;
-          resolve(data);
-          return;
+          if (!data.isLogin) {
+            bot_list[user_id] = null;
+          } else {
+            data.uuid = bot_list[user_id].uuid;
+            resolve(data);
+            return;
+          }
         }
         try {
           if (!bot_data) {
@@ -34,6 +38,10 @@ module.exports = app => {
         bot.PROP.uin ? bot.restart() : bot.start();
         bot.on('login', async () => {
           data.isLogin = true;
+          resolve(data);
+        });
+        bot.on('logout', async () => {
+          data.isLogin = false;
           resolve(data);
         });
         bot.on('error', err => {
